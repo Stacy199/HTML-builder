c441d57643be2140951dd13aeea0aa699a11e10d
Anastasia Blinova <nastia.blinova@gmail.com>
2025-01-20 16:17:46 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require('path');
+
+const filePath = path.join(__dirname, 'secret-folder');
+ 
+
+fs.readdir(filePath, (error, files) => {
+    if (error) {
+        return console.error('Error reading secret folder:', error);
+    }
+
+    files.forEach((file) => {
+        const folderPath = path.join(filePath, file);
+
+    fs.stat( folderPath, (error, stats) => {  
+        if (error)  {
+            return console.error(error);
+        }
+        if(stats.isFile()){
+            const fileExtention = path.extname(file).slice(1);
+            const sizeKB = (stats.size/1024).toFixed(3);
+            console.log(`${file} - ${fileExtention} - ${sizeKB}kb`);  
+        }
+    });
+});
+    
+});
